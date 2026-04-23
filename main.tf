18825dedef5b86b6c8a099f4e8be968659ce74c4
Pooja Tambe <poojatambe522@gmail.com>
2026-04-23 19:10:08 +0530

    added oter resource name

--- main.tf
+++ main.tf
@@ -72,7 +72,7 @@ resource "azurerm_subnet" "subnetb" {
   virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes = [ "20.0.0.0/24" ]
 }
-resource "azurerm_subnet" "subnetb" {
+resource "azurerm_subnet" "subnetb1" {
   name = "subnetb1"
   resource_group_name = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet1.name
