0856b8e2bda7926116669a764300b5f91f70c0cf
Pooja Tambe <poojatambe522@gmail.com>
2026-04-24 15:07:46 +0530

    added vm

--- main.tf
+++ main.tf
@@ -99,3 +99,25 @@ resource "azurerm_public_ip" "pip" {
   allocation_method = "Static"
   location = azurerm_resource_group.rg.location
 }
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name = "agent-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location = azurerm_resource_group.rg.location
+  size = "Standard_B1s"
+  admin_username = "adminuser"
+  admin_password = "password@2345!"
+  disable_password_authentication = true
+  network_interface_ids = [ azurerm_network_interface.nic.id ]
+
+  os_disk {
+    caching = ReadWrite
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer = "001-com-ubuntu-server-jammy"
+    sku = "22_04-lts"
+    version = "latest"
+  }
+}
