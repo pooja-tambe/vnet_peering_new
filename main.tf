5ef24c841bcd29e5221eb6889015be60f9376702
Pooja Tambe <poojatambe522@gmail.com>
2026-04-24 15:11:24 +0530

    corrected catching

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   network_interface_ids = [ azurerm_network_interface.nic.id ]
 
   os_disk {
-    caching = ReadWrite
+    caching = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
   source_image_reference {
